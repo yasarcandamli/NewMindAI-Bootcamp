71e2a854481bf8cc4c04da532fbec7799993b2c3
yasarcandamli <yasarcandamli@gmail.com>
2024-10-28 18:43:44 +0300

    Homework-2

---
+++ Live-Class/01-26.10.2024-Live-Class/basic-web-site/async.js
@@ -0,0 +1,5 @@
+// Js is a Single Thread
+
+console.log(1);
+console.log(2);
+
---
+++ Live-Class/01-26.10.2024-Live-Class/basic-web-site/script.js
@@ -0,0 +1,112 @@
+"use strict"
+
+// console.log('script executed from script.js');
+// alert('hello')
+
+// const pElement = document.getElementById('text');
+// console.log('pElement: ', pElement);
+
+// var username = 'Yasar Can';
+
+// let age = 27;
+
+// const lastname = "Damli";
+
+// username = 'Emre';
+// age = 28;
+// lastname = 'Banzaroglu';
+
+// console.log(username);
+// console.log(age);
+// console.log(lastname);
+
+// for (let i = 0; i < 5; i++) {
+// }
+
+// for (var i = 0; i < 5; i++) {
+// }
+
+// console.log(i);
+
+// let isSmoking = false;
+// console.log(isSmoking);
+// var isSmoking = false;
+// var isSmoking = true;
+
+// getName();
+// getNameArrow();
+
+// function getName(name = 'yasar can') {
+//     console.log('get name');
+//     console.log(name);
+// }
+
+// const getNameArrow = () => {
+//     console.log('get name');
+//     console.log(name);
+// }
+
+
+// string (primitive)
+// let name = 'yasar can';
+// const lastname = 'damli';
+// const company = 'Jotform';
+
+// console.log(typeof name);
+// console.log(typeof lastname);
+// console.log(typeof company);
+
+// // number (primitive)
+// const age = 15;
+// console.log(typeof age);
+
+// // boolean (primitive)
+// const isSmoking = false;
+// console.log(typeof isSmoking);
+
+// // null and undefined
+// var hobbies;
+// console.log(typeof hobbies);
+
+// const car = null;
+// console.log(typeof car);
+
+// // object (reference type)
+// const user = {
+//     name: 'mehmet',
+//     age: 25
+// }
+// console.log(typeof user);
+
+// // array (reference type)
+// const users = ['yusuf', 'kaan', 'sefa'];
+// console.log(typeof users);
+
+// let name = 'mehmet';
+// let nameCopy = name;
+
+
+// console.log('name', name);
+// console.log('nameCopy', nameCopy);
+
+// nameCopy = 'yusuf';
+
+// console.log('name', name);
+// console.log('nameCopy', nameCopy);
+
+// const users = ['nikolay', 'yusuf', 'özge'];
+
+// const copyUsers = users; // bu kopyalama değil
+
+// console.log(users);
+// console.log(copyUsers);
+
+// // copyUsers.push('sefa'); // mutation
+
+// // console.log(users);
+// // console.log(copyUsers);
+
+// const copyUsers2 = [...users];
+// console.log(users);
+// console.log(copyUsers);
+// console.log(copyUsers2);
